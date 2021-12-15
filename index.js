15ec90f73d0eb9013b9b44f7915baf1a2294f247
AsunaSemoulin <89163001+AsunaSemoulin@users.noreply.github.com>
2021-12-15 10:08:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PW = process.env.MONGO_PW;
 const MONGO_DB = process.env.MONGO_DB;
 
-const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PW}@cluster0.lropz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PW}@${MONGO_DB}.lropz.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
